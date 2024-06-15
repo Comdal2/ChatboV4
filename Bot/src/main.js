ba23d4c8526404acfc2bb61cc4dd9f9e4fdf86b7
Comdal <lucerodaniel99@yahoo.com>
2024-06-14 19:38:26 -0500

    first commit

---
+++ Bot/src/main.js
@@ -0,0 +1,28 @@
+import pkg from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const { Client, LocalAuth } = pkg;
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+      dataPath: "sessions",
+    }),
+    webVersionCache: {
+      type: "remote",
+      remotePath: 'https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2412.54v2.html',
+    },
+  });
+
+client.on('ready', () => {
+    console.log('Conexión Exitosa');
+    console.log('Cambios');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.initialize().catch((err) => {
+    console.error("Error al inicializar el cliente:", err);
+  });
+
